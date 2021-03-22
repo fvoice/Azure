557e17f0561e1871ed48fcfe0d23fb5e064fb9fd
fvoice <fvoice@ya.ru>
2021-03-22 21:58:17 +0100

    Service Bus Queue and Topic

---
+++ mslearn-connect-services-together/implement-message-workflows-with-service-bus/src/start/performancemessagesender/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace performancemessagesender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://fvoice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=LV9EZeG3jTx/eEJYUzrPRuiqxUTwGcFZftsd+KCq9IU=";
+        const string TopicName = "salesperformancemessages";
+        static ITopicClient topicClient;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sending a message to the Sales Performance topic...");
+
+            SendPerformanceMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+
+        }
+
+        static async Task SendPerformanceMessageAsync()
+        {
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            // Send messages.
+            try
+            {
+                string messageBody = $"Total sales for Brazil in August: $13m.";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                Console.WriteLine($"Sending message: {messageBody}");
+
+                await topicClient.SendAsync(message);
+
+                await topicClient.CloseAsync();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            // Close the connection to the topic here
+        }
+    }
+}
---
+++ mslearn-connect-services-together/implement-message-workflows-with-service-bus/src/start/privatemessagereceiver/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace privatemessagereceiver
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://fvoice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=LV9EZeG3jTx/eEJYUzrPRuiqxUTwGcFZftsd+KCq9IU=";
+        const string QueueName = "salesmessages";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+
+            ReceiveSalesMessageAsync().GetAwaiter().GetResult();
+
+        }
+
+        static async Task ReceiveSalesMessageAsync()
+        {
+
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        
+            Console.Read();
+
+            await queueClient.CloseAsync();
+
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }   
+    }
+}
